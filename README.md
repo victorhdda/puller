b7131d44302a3299dc4a15ce26d37b4d68381cec
Victor Davila <victorhdd@gmail.com>
2020-05-17 03:22:32 -0300

    puller v1

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
 # puller
 
+
 What I need:
 
-- pull a git project from a static url periodic: git@github.com:victorhdda/blog.git
-- rename and move specific files to a new local folder: all .md
-- add specific permissions to these files: -rw-r--r--
-
+- pull a git project from a static url periodic: git@github.com:victorhdda/blog.git - OK
+- rename and move specific files to a new local folder: all .md - OK
+- add specific permissions to these files: -rw-r--r-- - OK
+- create a cron job for executions
 
 Solutions:
 - create a cron job that executes these scripts periodic
-- git puller
-- files mover and permission
+- git puller - OK
+- files move and change permission
+
+find /tmp/aa02/ -type f -name '*.md' | xargs -I '{}' cp {} /tmp/aa02/folder
+
+ rename -n 's/(\d{4})-(\d{2})-(\d{2})-//g' *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## References
+
+https://www.tecmint.com/permanently-and-securely-delete-files-directories-linux/
+
+
+https://www.rexegg.com/regex-quickstart.html
+
+
+https://serverfault.com/questions/5685/renaming-files-in-linux-with-a-regex
--- puller.sh
+++ puller.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-dst_path="/tmp/aa03/"
+tmp_path=$1
+dst_path=$2
 
-tmp_path="/tmp/aa02/"
+echo $tmp_path
+echo $dst_path
+
+
+sleep 2
+#dst_path="/tmp/aa03/"
+#tmp_path="/tmp/aa02/"
 
 echo starting script...
 sleep 1
+# Delete and create directories
 
-find /tmp/aa02 -delete
+sudo find "$tmp_path""/" -delete
+sudo find "$dst_path""/" -delete
 
+#sleep 20
 
-sleep 1
-# Create temporary directory
+sleep 3
 mkdir "$tmp_path"
+mkdir "$dst_path"
 
-sleep 2
-echo temporary directory created
+
+#sleep 10
+echo temporary directories created
 
 # Destination folder should be empty
 #Cloning github
 
 echo Cloning repository
 
-git clone git@github.com:victorhdda/blog.git /tmp/aa02/
+git clone git@github.com:victorhdda/blog.git $tmp_path"/"
 
 wait
 
@@ -32,11 +43,10 @@ sleep 2
 #cd "$tmp_path"
 
 #touch oi.txt
-sleep 2
-find "$tmp_path" -type f -name '*.md' | xargs -I '{}' cp {} "$dst_path" #dest_folder
+#sleep 2
+find "$tmp_path""/" -type f -name '*.md' | xargs -I '{}' cp {} "$dst_path" #dest_folder
 
 #copiar imagens cp -R images/* folder/
-
 
 
 
@@ -44,10 +54,10 @@ sleep 2
 cd "$dst_path"
 echo cd..
 
-sleep 1
+#sleep 1
 rename 's/(\d{4})-(\d{2})-(\d{2})-//g' *.md
 
-sleep 20
+sleep 2
 #sudo chmod a-rwx 2018-09-23-vpn.md - remove todas as permissões do arquivo
 #sudo chmod a+r 2018-09-23-vpn.md  - adiciona permissões somente de leitura
 
@@ -57,10 +67,17 @@ sleep 20
 
 # sudo chmod 644 -R /tmp/aa02/folder/* change permission
 
-sudo chmod 644 -R "$dst_path"*
+#Mover imagens
+
+#path posts: /home/vh/Blog/source/_posts
+
+#path images: /home/vh/Blog/source/images
+
+cp -R "$tmp_path"/images/* "$dst_path"
 
 
 
+sudo chmod 744 -R "$dst_path"*
 
 
 
